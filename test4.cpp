d4fcb98ff405b034a21cbc2ac60187d340140cd3
Rizkitul <muhammadrizkiakbar@yahoo.com>
2016-12-05 17:44:13 +0700

    latihan

---
+++ test4.cpp
@@ -0,0 +1,17 @@
+#include	<stdio.h>
+#include	<conio.h>
+#include <iostream.h>
+
+main()
+{
+float tugas,absen,uts,uas,na;
+
+cout	<<"MASUKAN NILAI MAHASISWA"<<endl;
+cout	<<"***********************"<<endl;
+cout	<<"masukan nilai tugas	: "<<endl;
+cout	<<"masukan nilai absen	: "<<endl;
+cout	<<"masukan nilai uts		: "<<endl;
+cout	<<"masukan nilai uas		: "<<endl;
+
+
+
