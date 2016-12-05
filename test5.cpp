6b1d3494a9484e499b5d0ddb822a7e118003a7f5
Rizkitul <muhammadrizkiakbar@yahoo.com>
2016-12-05 17:47:31 +0700

    latihan

---
+++ test5.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream.h>
+main()
+{
+	int a,b;
+	float k1,k2,k3,k4,k5,k6;
+
+	cout	<<"Masukan Kondisi	1	:";cin>>a;
+   cout	<<"Masukan Kondisi	2	:";cin>>b;
+
+	k1=	a	==	b;
+	k2=	a	>	b;
+	k3=	a	>=	b;
+	k4=	a	<	b;
+	k5=	a	<=	b;
+	k6=	a	!=	b;
+
+
+
+		cout<<" HASIL PERBANDINGAN ADALAH "<<endl;
+	cout<<"Hasil dari"<<x<<"==<<y<<"="<<a<<endl;
+	cout<<"hasil dari"<<x<<"!=<<y<<"="<<b<<endl;
+	cout<<"hasil dari"<<x<<">"<<y<<"="<<c<<endl;
+	cout<<"hasil dari"<<x<<"<"<<y<<"="<<d<<endl;
+	cout<<"hasil dari"<<x<<">="<<y<<"="<<e<<endl;
+	cout<<"hasil dari"<<x<<"<=<<y<<"="<<f<<endl;
+getch();
+}
